0e9f7013ecda7bd2dfe0ac08dc967afbde8eac15
Silva <wesley.santos.silva@ccc.ufcg.edu.br>
2021-12-10 09:01:01 -0300

    transforming data with pipes

--- APM-Start/src/app/products/product-list-component.ts
+++ APM-Start/src/app/products/product-list-component.ts
@@ -5,7 +5,11 @@ import { Component } from "@angular/core";
   templateUrl: './product-list-component.html',
 })
 export class ProductListComponent {
-  pageTitle: string = 'Product List';
+  pageTitle = 'Product List';
+  imageWidth = 50;
+  imageMargin = 2;
+  showImage = false;
+  listFilter = 'cart';
   products: any[] = [
     {
       productId: 1,
@@ -58,4 +62,8 @@ export class ProductListComponent {
       imageUrl: 'assets/images/xbox-controller.png',
     },
   ];
+
+  toggleImage(): void {
+    this.showImage = !this.showImage;
+  }
 }
